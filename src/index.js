3cf0be8f0084fe720a7939ab346b7ba9f5cd1c75
vinicozzi <vinicozzimincent@gmail.com>
2023-05-02 14:05:21 -0400

    newGame

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 // Add your import statements for View and Game here
+// import View from "./ttt-view.js";
+// import Game from "./game.js";
 
-import View from "./ttt-view.js"
-import Game from "./game.js"
+// import View from './ttt-view/view.js';
+// import Game from './ttt_node/game.js';
+
+// import View from './ttt-view.js';
+// import Game from './game.js';
+
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const view = new View();
 });
