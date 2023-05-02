e03443b860dc361efa30865fb682bba5c1c07763
Naoko Sugawara <infoseek122@gmail.com>
2023-05-02 13:52:09 -0400

    started to import files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+
+import View from "./ttt-view.js"
+import Game from "./game.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
-});sfdfgerhd
+});
